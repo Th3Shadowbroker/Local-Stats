d981de0216df349488de506021e0c82b240f000e
Jens Fischer <shadowbroker@crafter629.de>
2016-04-24 09:22:10 +0200

    Implemented experimental update-notification

---
+++ src/com/th3shadowbroker/loc/update/NotifyUpdate.java
@@ -0,0 +1,40 @@
+package com.th3shadowbroker.loc.update;
+
+import com.th3shadowbroker.loc.sys.ColorCode;
+import com.th3shadowbroker.locs.main;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class NotifyUpdate implements Listener{
+    
+    private main plugin;
+    
+    public NotifyUpdate(main plugin)
+    {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onPlayerJoinUpdateCheck(PlayerJoinEvent ev)
+    {
+        Player p = ev.getPlayer();
+        UpdateChecker upc = new UpdateChecker(plugin, "http://dev.bukkit.org/bukkit-plugins/localstats/file.rss");
+        
+        if ( p.isOp() )
+        {
+            
+            if ( upc.updateAvailable() )
+            {
+                
+                p.sendMessage(plugin.prefix + ColorCode.CC("&ePlease update LOCStats !"));
+                p.sendMessage(plugin.prefix + ColorCode.CC("Download the newest version from http://bit.ly/localstats"));
+                
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ src/com/th3shadowbroker/loc/update/UpdateChecker.java
@@ -0,0 +1,63 @@
+package com.th3shadowbroker.loc.update;
+
+import com.th3shadowbroker.locs.main;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class UpdateChecker {
+     
+    private main plugin;
+	private URL feed;
+	
+	private String version;
+	private String link;
+	
+	public UpdateChecker(main plugin,String url){
+		this.plugin = plugin;
+		
+		try {
+			this.feed = new URL(url);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public boolean updateAvailable(){
+		
+		try {
+			InputStream In = feed.openConnection().getInputStream();
+			Document xmlDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(In);
+			
+			Node latestFile = xmlDoc.getElementsByTagName("item").item(0);
+			NodeList children = latestFile.getChildNodes();
+			
+			version = children.item(1).getTextContent().replaceAll("[a-zA-Z ]", "");
+			link = children.item(3).getTextContent();
+			
+			if(! (version.equals(plugin.getDescription().getVersion()) )){
+				return true;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+	public String getDlVersion(){
+		return version;
+	}
+	
+	public String getDownloadURL(){
+		return link;
+	}
+    
+}
